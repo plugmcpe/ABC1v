4ccd7e4d2493dde649850c09cac6d26b9e0345a9
plugmcpe <M7mmdgamer7@icloud.com>
2017-07-02 07:05:02 +0300

    Create Main.php

---
+++ src/ABC1v/Main.php
@@ -0,0 +1,23 @@
+<?php
+namespace ABC1v;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+class Main extends PluginBase{
+
+public function onEnable(){
+ $this->getServer()->getLogger()->info("plugin ABC1v has been enable");
+ $this->getLogger()->info("hello youtube");
+  }
+  public function onDisable(){
+   $this->getServer()->getLogger()->info(plugin ABC1v has been disable);
+    }
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+     switch ($cmd->getName()){
+      case '1':
+       $player = $sender->setGamemode(1);
+       $player->setGamemode(1);
+        break;
+        }
+       }
+      }
